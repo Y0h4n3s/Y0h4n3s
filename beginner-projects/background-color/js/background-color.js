4005922c93e4b7b54d7c353a332bf91044c6b855
Y0h4n3s <yohaksa80@gmail.com>
2020-10-13 14:58:04 -0400

    revesion

--- beginner-projects/background-color/js/background-color.js
+++ beginner-projects/background-color/js/background-color.js
@@ -1,5 +1,12 @@
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
-
 function clicked(button) {
-    document.querySelector('#cont').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    r = Math.floor(Math.random() * 255)
+    g = Math.floor(Math.random() * 255)
+    b = Math.floor(Math.random() * 255)
+    document.querySelector('#cont').style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    document.querySelector('.r-value').innerHTML = r
+    document.querySelector('.g-value').innerHTML = g
+    document.querySelector('.b-value').innerHTML = b
+    document.querySelector('.rgb-values').style.display = 'inline-block';
+    document.querySelector('.rgb-values').style.color = `rgb(${r}, ${g}, ${b})`;
+    document.querySelector('.display').style.backgroundColor = `rgb(${255 - r}, ${255 - g}, ${255 - b})`;
 }
